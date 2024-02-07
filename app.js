0cf2c90777422a65059470fb9d83833362a28e76
Luke Gaunt <30408050+FinchyG@users.noreply.github.com>
2024-02-07 07:24:53 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Continents Highest Mountains REST API Router Example');
 });
 
 const port = process.env.PORT || 3000;
